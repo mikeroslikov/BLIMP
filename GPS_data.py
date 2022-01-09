32ceb369da010d57888278a189d3ea4040530a38
mikeroslikov <MAlphaRomeo@protonmail.com>
2022-01-08 17:25:52 -0800

    Automated Commit

--- GPS_data.py
+++ GPS_data.py
@@ -4,7 +4,11 @@ Reading/Parsging GPS data over Serial
 """
 
 import serial
-ser=serial.Serial(’/dev/ttyACM0’,9600)
-readedText = ser.readline()
-print(readedText)
+ser=serial.Serial("/dev/ttyACM0",9600)
+
+while True:
+    readedText = ser.readline()
+    print(readedText)
+
+
 ser.close()
