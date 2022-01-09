cc20aae1de9fe25c407f5e48684c7d92cf7c950d
mikeroslikov <MAlphaRomeo@protonmail.com>
2022-01-08 22:53:34 -0800

    Automated Commit

--- i2c_rip.py
+++ i2c_rip.py
@@ -4,6 +4,6 @@ import re
 data=""
 i=''
 for i in range(0,256):
-    data+=subprocess.check_output("i2cget -y 1 0x1e "+hex(i),stderr=subprocess.STDOUT, shell = True)
+    data+=str(subprocess.check_output("i2cget -y 1 0x1e "+hex(i),stderr=subprocess.STDOUT, shell = True))
     #data.append()
 print(re.findall(r"0x..", data))
