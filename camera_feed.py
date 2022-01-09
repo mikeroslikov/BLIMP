7a34d4b12dd81134f3e5ba9a45671d5df7201cfa
mikeroslikov <MAlphaRomeo@protonmail.com>
2022-01-08 18:42:28 -0800

    Automated Commit

--- camera_feed.py
+++ camera_feed.py
@@ -3,12 +3,12 @@ from time import sleep
 import os
 import datetime
 
-DATE = datetime.datetime.today().strftime("%d-%B-%Y_%H:%M")
+DATE = datetime.datetime.today().strftime("%d-%B-%Y_%H_%M")
 DIRECTORY = '/home/pi/REC/'
 
 os.system("mkdir "+DIRECTORY)
 i=1
 while i<13:
     print("libcamera-vid -t 3000 -o "+DIRECTORY+DATE+".h264")
-    os.system("libcamera-vid -t 3000 -o "+DIRECTORY+DATE+".h264")
+    os.system("libcamera-vid -t 300000 -o "+DIRECTORY+DATE+".h264")
     i+=1
