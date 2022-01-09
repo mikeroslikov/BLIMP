3e15dc38d879bf73ae3dad960e6aa705b3cc0fb9
mikeroslikov <MAlphaRomeo@protonmail.com>
2022-01-08 23:14:37 -0800

    Automated Commit

--- i2c_rip.py
+++ i2c_rip.py
@@ -6,13 +6,13 @@ data1=""
 data2=""
 
 for i in range(0,256):
-    data1+=str(subprocess.check_output("i2cget -y 1 0x1e "+hex(i),stderr=subprocess.STDOUT, shell = True))
+    data1+=str(subprocess.check_output("i2cget -y 1 0x1e "+hex(i)+" w",stderr=subprocess.STDOUT, shell = True))
 time.sleep(1)
 for i in range(0,256):
-    data2+=str(subprocess.check_output("i2cget -y 1 0x1e "+hex(i),stderr=subprocess.STDOUT, shell = True))
+    data2+=str(subprocess.check_output("i2cget -y 1 0x1e "+hex(i)+" w",stderr=subprocess.STDOUT, shell = True))
 time.sleep(1)
-a1=re.findall(r"0x..", data1)
-a2=re.findall(r"0x..", data2)
+a1=re.findall(r"0x....", data1)
+a2=re.findall(r"0x....", data2)
 
 for j in range(0,len(a1)):
     if a1[j] != a2[j]:
