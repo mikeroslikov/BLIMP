40ccae9e60cf63586af0416a59889d9a89686b86
mikeroslikov <MAlphaRomeo@protonmail.com>
2022-01-08 18:08:15 -0800

    Automated Commit

--- camera_feed.py
+++ camera_feed.py
@@ -1,12 +1,12 @@
 from picamera import PiCamera
 from time import sleep
-from os import system
+import pathlib
 from time import sleep
 import datetime
 
 DATE = datetime.datetime.today().strftime("%d-%B-%Y_%H:%M")
 DIRECTORY = '/home/pi/REC/' + DATE
-os.mkdir(DIRECTORY)
+pathlib.Path(DIRECTORY).mkdir(parents=True, exist_ok=True) 
 camera = PiCamera()
 
 camera.start_recording(DIRECTORY + '/TIMG_{0:05d}.h264'.format(i))
