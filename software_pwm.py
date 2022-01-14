30f9bd660f9c829688fd3d948c3e3d61039afc0b
mikeroslikov <MAlphaRomeo@protonmail.com>
2022-01-13 23:47:50 -0800

    Automated Commit

--- software_pwm.py
+++ software_pwm.py
@@ -9,22 +9,13 @@ p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(2.5) # Initialization
 try:
   while True:
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
+	for i i range(0,100)
+		p.ChangeDutyCycle(i/10)
+		time.sleep(0.1)
+	for i i range(100,0)
+		p.ChangeDutyCycle(i/10)
+		time.sleep(0.1)
+    
 except KeyboardInterrupt:
   p.stop()
   GPIO.cleanup()
