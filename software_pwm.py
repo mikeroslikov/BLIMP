c3fda7246ae70199a2adcea89c01d7c7fa0696ab
mikeroslikov <MAlphaRomeo@protonmail.com>
2022-01-13 23:49:40 -0800

    Automated Commit

--- software_pwm.py
+++ software_pwm.py
@@ -9,10 +9,10 @@ p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(2.5) # Initialization
 try:
   while True:
-    for i in range(0,100)
+    for i in range(0,100):
 	    p.ChangeDutyCycle(i/10)
 	    time.sleep(0.1)
-    for i in range(100,0)
+    for i in range(100,0):
 	    p.ChangeDutyCycle(i/10)
 	    time.sleep(0.1)
     
