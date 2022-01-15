e11c3c85b50080da8b0dfdb603d27d5e5bd95404
mikeroslikov <MAlphaRomeo@protonmail.com>
2022-01-15 00:02:00 -0800

    Automated Commit

---
+++ pwm_hardware.py
@@ -0,0 +1,13 @@
+from adafruit_servokit import ServoKit
+kit = ServoKit(channels=16)
+
+kit.servo[0].actuation_range = 180
+kit.servo[0].set_pulse_width_range(1000, 2000)
+
+while True:
+    for i in range(0,180):
+	    kit.servo[0].angle = i
+	    time.sleep(0.1)
+    for i in range(180,0):
+	    kit.servo[0].angle = i
+	    time.sleep(0.1)
